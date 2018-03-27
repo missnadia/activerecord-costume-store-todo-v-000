2235f09b948e4059bc4e6006d9022e3307cd0467
Nadia Lee <miss.nadia.lee@gmail.com>
2018-03-27 01:50:02 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,8 +6,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.string :theme
       t.integer :price
       t.boolean :family_friend
-      t.date :opening_date
-      t.date :closing_date
+      t.datetime :opening_date
+      t.datetime :closing_date
       t.string :description
   end
 end
