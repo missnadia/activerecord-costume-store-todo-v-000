4529995d789787b70b4d3f1099587e92e700b97f
Nadia Lee <miss.nadia.lee@gmail.com>
2018-03-27 01:58:54 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -5,7 +5,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.float :price
       t.string :size
       t.string :image_url
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
