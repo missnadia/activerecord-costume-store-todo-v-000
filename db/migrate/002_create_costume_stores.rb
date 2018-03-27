859a0a3ddf4b4238b6f07d18f70c99ce7063f30d
Nadia Lee <miss.nadia.lee@gmail.com>
2018-03-27 01:45:18 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,12 @@
-# Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :employees
+      t.string :in_business
+      t.time :opening_time
+      t.time :closing_time
+  end
+end
